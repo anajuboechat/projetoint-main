be30eb3647cec1a097ead52cdf57876681f46247
Anaju <anajuboechat@gmail.com>
2025-10-30 08:24:17 -0300

    one

---
+++ app/src/main/java/com/projeto/medvest/ui/auth/LoginFragment.kt
@@ -0,0 +1,52 @@
+package com.projeto.medvest.ui.auth
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.projeto.medvest.R
+import com.projeto.medvest.databinding.FragmentLoginBinding
+import com.projeto.medvest.util.showBottomSheet
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListener()
+    }
+
+    private fun initListener(){
+        binding.buttonLogin.setOnClickListener {
+            valideData()
+        }
+        binding.textViewCriarConta.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+        binding.textViewRecuperarConta.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_recoverAccountFragment)
+        }
+    }
+
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
